b1fab346cb6c7405a2d533c8c6b62d826fe6c683
Kalavrat <97897883+Kalavrat@users.noreply.github.com>
2022-03-04 06:41:24 +0300

    Add files via upload

---
+++ sobitiya/main.js
@@ -0,0 +1,52 @@
+//Квадрат
+
+// function kvadrat() {
+//   let square = document.querySelector(`.square`);
+//   square.style.background = "red";
+// }
+
+//1
+
+// function Hi() {
+//   alert("Привет мир!");
+// }
+
+// 2
+
+// function inp() {
+//   let input = document.querySelector(".input");
+//   input.value = "test@email.ru";
+// }
+
+//3
+
+// function text() {
+//   let input = document.querySelector(".input");
+//   let vvod = input.value;
+//   if (vvod == "") {
+//     alert("Вы ничего не ввели в поле");
+//   } else {
+//     alert(`Вы ввели ${vvod}`);
+//   }
+// }
+
+//4
+
+// function swap() {
+//   let input1 = document.querySelector(".input1");
+//   let input2 = document.querySelector(".input2");
+//   let per = input1.value;
+//   input1.value = input2.value;
+//   input2.value = per;
+// }
+
+//5
+
+// function ban() {
+//   let input = document.querySelector(".input");
+//   input.disabled = true;
+// }
+// function unban() {
+//   let input = document.querySelector(".input");
+//   input.disabled = false;
+// }
